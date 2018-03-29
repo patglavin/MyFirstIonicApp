327fe8b367ce484057091b2baa04f1701439791c
Patrick Glavin <patjglavin@gmail.com>
2018-03-29 18:38:57 -0400

    stashing

---
+++ src/pages/BasicProfile/BasicProfile.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+    selector: 'basic-profile',
+    templateUrl: 'BasicProfile.html'
+})
+export class BasicProfile {
+
+    constructor(public navCtrl: NavController) {
+
+    }
+
+}
